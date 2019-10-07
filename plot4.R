df0bb7f871bfb14c79090c365388409d88520f17
kamyzhu <zhukehk@hotmail.com>
2019-10-08 00:11:40 +0800

    updates plot4 and its script

--- plot4.R
+++ plot4.R
@@ -19,6 +19,6 @@ with(epcsub, plot(datetime, Sub_metering_1, type="l", xlab="", ylab="Energy sub 
 with(epcsub, lines(datetime, Sub_metering_2, col="red"))
 with(epcsub, lines(datetime, Sub_metering_3, col="blue"))
 legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
-       col=c("black", "red","blue"), lwd=1)
+       col=c("black", "red","blue"), lwd=1, bty="n")
 with(epcsub, plot(datetime, Global_reactive_power, type="l"))
 dev.off()
